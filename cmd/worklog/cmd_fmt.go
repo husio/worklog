4f00b1d9fceda2863788cd5d2b1eb0ba70b327dc
Piotr Husiatyński <phusiatynski@gmail.com>
2021-08-04 21:20:52 +0200

    fmt html is now available
    Port HTML rendering for the fmt command from the webui.

--- cmd/worklog/cmd_fmt.go
+++ cmd/worklog/cmd_fmt.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"encoding/csv"
 	"encoding/json"
 	"errors"
@@ -8,6 +10,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"text/template"
+	"time"
 
 	"github.com/husio/worklog/wlog"
 )
@@ -69,7 +74,70 @@ func cmdFmt(input io.Reader, output io.Writer, args []string) error {
 			}
 		}
 		return nil
+	case "html":
+		var extended []*wlog.Entry
+		if len(entries) > 0 {
+			var i int
+			for t := entries[0].Day; !t.After(entries[len(entries)-1].Day); t = t.Add(time.Hour * 24) {
+				if i < len(entries) && entries[i].Day.Equal(t) {
+					extended = append(extended, entries[i])
+					i++
+				} else {
+					extended = append(extended, &wlog.Entry{Day: t})
+				}
+			}
+		}
+		sort.Slice(extended, func(i, j int) bool {
+			return extended[i].Day.After(extended[j].Day)
+		})
+
+		var byMonth [][]*wlog.Entry
+		if len(extended) > 0 {
+			current := []*wlog.Entry{extended[0]}
+			for _, e := range extended[1:] {
+				if e.Day.Month() == current[0].Day.Month() {
+					current = append(current, e)
+				} else {
+					byMonth = append(byMonth, current)
+					current = []*wlog.Entry{e}
+				}
+			}
+			byMonth = append(byMonth, current)
+		}
+		context := struct {
+			Entries [][]*wlog.Entry
+		}{
+			Entries: byMonth,
+		}
+		var b bytes.Buffer
+		if err := fmtTmpl.Execute(&b, context); err != nil {
+			return fmt.Errorf("render template: %w", err)
+		}
+		if _, err := b.WriteTo(output); err != nil {
+			return fmt.Errorf("write to output: %w", err)
+		}
+		return nil
 	default:
 		return errors.New("valid formats are text, json, csv")
 	}
 }
+
+//go:embed cmd_fmt.html
+var htmlFmtTemplate string
+
+var fmtTmpl = template.Must(template.New("").Funcs(template.FuncMap{
+	"narrowhours": func(d time.Duration) string {
+		hours := d / time.Hour
+		if hours == 0 {
+			return ""
+		}
+		return fmt.Sprintf("%dh", hours)
+	},
+	"hoursduration": func(entries []*wlog.Entry) uint {
+		var total time.Duration
+		for _, e := range entries {
+			total += e.TotalDuration()
+		}
+		return uint(total.Hours())
+	},
+}).Parse(htmlFmtTemplate))
